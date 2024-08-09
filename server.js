9b61cb25047a2b19658b8c57e6c811be9d18cefd
kresna27 <kresnaaprianto87@gmail.com>
2024-08-09 13:37:03 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+import { server as _server } from '@hapi/hapi';
+
+const init = async () => {
+
+    const server = _server({
+        port: 5000,
+        host: 'localhost'
+    });
+
+    // root path
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return {
+                'msg': 'hello world'
+            };
+        }
+    });
+
+    server.route({
+        method:'GET',
+        path: '/profile',
+
+        handler:(request,h)=>{
+            return {
+                'name': 'kresna aprianto',
+                'address': 'kampungbaru nganjuk',
+                'phone number' : '9478342323'
+            }
+        }
+    })
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
